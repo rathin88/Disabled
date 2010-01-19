e95497d00c165abf521baf5db317564e8d214f78
Cyriac <cyriacsmail@gmail.com>
2010-01-19 11:29:48 +0530

    User authentication and clutter removed

--- openhappiness/searchnstore/search/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from search.models import User
-from search.models import Tag
-from search.models import Follow
-
-admin.site.register(User)
-admin.site.register(Tag)
-admin.site.register(Follow)
